f1f11e767ec4774eb054ac226261685c9ac768cb
Gabriel Medeiros <128101666+Gaab19@users.noreply.github.com>
2025-06-02 21:16:24 -0300

    Delete Atividades e Aprendizado directory

--- Atividades e Aprendizado/ConversorMoeda.java
+++
@@ -1,9 +0,0 @@
-public class ConversorMoeda {
-   public static void main (String [] args) {
-      double dolar = 100.0D;
-      double taxaCambio = 5.20D;
-      double valorConvertido = dolar * taxaCambio;
-
-      System.out.println("O valor convertido de dólar para Reais é: " + valorConvertido);
-   }
-}
